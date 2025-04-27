f397016842e4d689e2cecd9f803bb5fb034f26ef
bachhieu <bachviethieu@gmail.com>
2025-04-27 17:24:42 +0700

    init base docker file and example docker compose

--- server/users/user.required_login_handler.go
+++ server/users/user.required_login_handler.go
@@ -1,8 +1,6 @@
 package users
 
 import (
-	"fmt"
-
 	"github.com/bachhieu/fountain/baselib/auth_token"
 	"github.com/bachhieu/fountain/baselib/v_net/v_api"
 	"github.com/bachhieu/fountain/proto/v_proto"
@@ -13,7 +11,6 @@ func (api *UsersAPI) RequiredLogin(ctx *fiber.Ctx) error {
 	_, _, accountID, _, _, _, _, err := auth_token.ExtractToken(ctx, false)
 
 	if err != nil {
-		fmt.Println("le::::", err)
 		return v_api.WriteError(ctx, v_proto.NewRpcError(int32(v_proto.VolioRpcErrorCodes_BAD_REQUEST), "Token invalid or expired!"))
 	}
 
